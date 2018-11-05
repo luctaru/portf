8c2662a803b008df0dce4e63d73ab23618cfce21
luctaru <lucastarumoto@gmail.com>
2018-11-05 14:50:45 -0200

    Updates

---
+++ precache-manifest.a15cb415cda060ea5cea43dcd5c983c0.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "4c00d43bd72079d886f14b1cc8bef6e3",
+    "url": "/portf/static/media/bioinfo-img.4c00d43b.svg"
+  },
+  {
+    "revision": "71f0965f4145bf574a03",
+    "url": "/portf/static/css/main.8fa23a5b.chunk.css"
+  },
+  {
+    "revision": "3a8bd219046a470c4da5",
+    "url": "/portf/static/js/1.3a8bd219.chunk.js"
+  },
+  {
+    "revision": "b5b818223398aa2b3174",
+    "url": "/portf/static/js/runtime~main.b5b81822.js"
+  },
+  {
+    "revision": "4e35ae82e1579bb98e99ca90eca60ad5",
+    "url": "/portf/static/media/profile-img.4e35ae82.svg"
+  },
+  {
+    "revision": "6e09dbd2e42ea1f1e5921258ce45faff",
+    "url": "/portf/static/media/curriculum.6e09dbd2.pdf"
+  },
+  {
+    "revision": "db921edc1f5cbd101eff8f0d99b398df",
+    "url": "/portf/static/media/profile-icon.db921edc.svg"
+  },
+  {
+    "revision": "403d58b40f28bc5a1e9ba66861f21ef6",
+    "url": "/portf/static/media/wrench-icon.403d58b4.svg"
+  },
+  {
+    "revision": "7a8eff20c0484391186da3ab86291ac0",
+    "url": "/portf/static/media/blog-icon.7a8eff20.svg"
+  },
+  {
+    "revision": "8ef5449716a2e3892549648ed614dbdf",
+    "url": "/portf/static/media/no-mans-sky-art.8ef54497.svg"
+  },
+  {
+    "revision": "71f0965f4145bf574a03",
+    "url": "/portf/static/js/main.71f0965f.chunk.js"
+  },
+  {
+    "revision": "f813280770917152c656143e21bbbd65",
+    "url": "/portf/static/media/tableless_logo.f8132807.svg"
+  },
+  {
+    "revision": "560f61c45fb7be0fc13f06235a33104a",
+    "url": "/portf/static/media/udemy-logo.560f61c4.svg"
+  },
+  {
+    "revision": "c26a73a25e9f56e7eff6b8ff05c58084",
+    "url": "/portf/static/media/book-icon.c26a73a2.svg"
+  },
+  {
+    "revision": "b4310dc26396136de1c9ce9da33bfde1",
+    "url": "/portf/static/media/gear-icon.b4310dc2.svg"
+  },
+  {
+    "revision": "85d8c9c02d49361007f0b8ce9a8f0929",
+    "url": "/portf/static/media/about-icon.85d8c9c0.svg"
+  },
+  {
+    "revision": "5998c9f46c1e2835aeb732aacb59ff26",
+    "url": "/portf/static/media/phone-icon.5998c9f4.svg"
+  },
+  {
+    "revision": "46c285471304bcd5ddcf7337736e9e0a",
+    "url": "/portf/static/media/github-icon.46c28547.svg"
+  },
+  {
+    "revision": "bc47ee5344e19fce0a1a060548990ac3",
+    "url": "/portf/static/media/facebook-icon.bc47ee53.svg"
+  },
+  {
+    "revision": "9e513dc1371c02ad2dc50162e98a42d6",
+    "url": "/portf/index.html"
+  }
+];
